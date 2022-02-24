78a3d6f46060dbfe4a6091e2ce65f92a219a1145
ualiangzhang <68933075+ualiangzhang@users.noreply.github.com>
2022-02-24 15:36:55 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -15,14 +15,13 @@ void demorgan() {
     solver s(c);
     // adding the negation of the conjecture as a constraint.
     s.add(!conjecture);
-    std::cout << "de-Morgan example\n";
-//    std::cout << s << "\n";
-//    std::cout << s.to_smt2() << "\n";
-//    switch (s.check()) {
-//        case unsat:   std::cout << "de-Morgan is valid\n"; break;
-//        case sat:     std::cout << "de-Morgan is not valid\n"; break;
-//        case unknown: std::cout << "unknown\n"; break;
-//    }
+    std::cout << s << "\n";
+    std::cout << s.to_smt2() << "\n";
+    switch (s.check()) {
+        case unsat:   std::cout << "de-Morgan is valid\n"; break;
+        case sat:     std::cout << "de-Morgan is not valid\n"; break;
+        case unknown: std::cout << "unknown\n"; break;
+    }
 }
 
 int main() {
