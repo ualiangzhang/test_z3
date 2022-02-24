ba0a6c8c1fbd632acc98ffbe8cb55733875e81f6
ualiangzhang <68933075+ualiangzhang@users.noreply.github.com>
2022-02-24 15:36:01 -0700

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include<vector>
+#include "z3++.h"
+using namespace z3;
+
+void demorgan() {
+    std::cout << "de-Morgan example\n";
+
+    context c;
+
+    expr x = c.bool_const("x");
+    expr y = c.bool_const("y");
+    expr conjecture = (!(x && y)) == (!x || !y);
+
+    solver s(c);
+    // adding the negation of the conjecture as a constraint.
+    s.add(!conjecture);
+    std::cout << "de-Morgan example\n";
+//    std::cout << s << "\n";
+//    std::cout << s.to_smt2() << "\n";
+//    switch (s.check()) {
+//        case unsat:   std::cout << "de-Morgan is valid\n"; break;
+//        case sat:     std::cout << "de-Morgan is not valid\n"; break;
+//        case unknown: std::cout << "unknown\n"; break;
+//    }
+}
+
+int main() {
+    try{
+        demorgan();
+        std::cout << "\n";
+    }
+    catch (exception & ex) {
+        std::cout << "unexpected error: " << ex << "\n";
+    }
+
+    return 0;
+}
